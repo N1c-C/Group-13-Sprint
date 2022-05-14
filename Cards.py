0ca10b89952f95b3428295527ed2b721248c30a3
N1c-C <103114303+N1c-C@users.noreply.github.com>
2022-05-14 13:04:46 +0100

    Add files via upload

---
+++ Cards.py
@@ -0,0 +1,49 @@
+"""
+Card Classes for Group 13 Sprint
+"""
+
+import random
+
+
+class Card:
+    """ Represents a single playing card
+    param: suit (str) H,D,C or S: On instantiation assigned to self.suit
+    param: value (int) 1-13: On instantiation assigned to self.value"""
+
+    def __init__(self, suit, value):
+        self.suit = suit
+        self.value = value
+
+
+class Deck:
+    """ Represents a pack of 52 Card objects
+     On instantiation 52 Card objets are added to the internal _deck list
+     param: self._deck: List holding the current Card objects in the Deck"""
+
+    def __init__(self):
+        self._deck = []
+        for suit in ['H', 'D', 'S', 'C']:
+            for value in range(1, 14):
+                self._deck.append(Card(suit, value))
+
+    def shuffle(self):
+        """Randomly shuffles self._deck"""
+        random.shuffle(self._deck)
+
+    def deal(self):
+        """ Removes and returns the first Card object in the self._deck list"""
+        return self._deck.pop(0)
+
+    def count(self):
+        """Returns the number of Cards objects in self._deck"""
+        return len(self._deck)
+
+    def shuffle_fy(self):
+        """ Shuffles the deck using the Fisher-Yates shuffling algorithm.
+        A randomly selected kth card is swapped with the nth card
+        param: n decremental index of all Card objects in current deck
+        param: k random Card Selection"""
+
+        for n in range(self.count()-1, -1, -1):
+            k = random.randint(0, n)
+            self._deck[k], self._deck[n] = self._deck[n], self._deck[k]
---
+++ card_tests.py
@@ -0,0 +1,32 @@
+# Tests to demonstrate Card related classes
+
+
+from Cards import Deck
+
+card_deck = Deck()
+
+
+def display_deck(deck):
+    for i in range(deck.count()):
+        card = deck.deal()
+        print(f'{card.suit}{card.value} ', end='')
+
+
+# Check there are 52 cards, deal and display cards:
+print(f'Cards in pack: {card_deck.count()}')
+display_deck(card_deck)
+
+# Shuffle the pack then display cards
+card_deck = Deck()
+card_deck.shuffle()
+
+print('\n\nCard order after a random shuffle')
+display_deck(card_deck)
+
+# For fun:shuffle the pack using fisher-yates algorithm
+
+card_deck = Deck()
+card_deck.shuffle_fy()
+
+print('\n\nCard order after Fisher-Yates shuffle')
+display_deck(card_deck)
