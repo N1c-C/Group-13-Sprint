7d1411c7a0ad7ba3ace4d15b44f4b9cf0f5222ae
N1c-C <103114303+N1c-C@users.noreply.github.com>
2022-05-15 22:24:16 +0100

    Delete Hand.py

--- Hand.py
+++
@@ -1,7 +0,0 @@
-from Cards import *
-from Deck import *
-
-class Hand(Deck):
-    """"a hand of cards. Either player or dealer"""
-    def __init__(self):
-        self.cards = []                         # initially no cards dealt
